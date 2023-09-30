12f6fe8f26f679712d73578a5d648e81fd68f81f
devilkiller-ag <ashmitgupta.official@gmail.com>
2023-10-01 01:59:36 +0530

    created level tiles

--- code/editor.py
+++ code/editor.py
@@ -9,8 +9,10 @@ from menu import Menu
 
 class Editor:
 	def __init__(self):
+
 		## main setup 
 		self.editor_display_surface = pygame.display.get_surface()
+		self.canvas_data = {}
 
 		## navigation
 		self.origin = vector()
@@ -24,10 +26,26 @@ class Editor:
 
 		## selection: what type of object/data is selected by the player in the editor
 		self.selection_index = 2 # By Default set to 2, i.e., terrain
+		self.last_selected_cell = None
 
 		## menu
 		self.menu = Menu()
 
+	### SUPPORT FUNCTIONS
+	def get_current_cell(self):
+		distance_to_origin = vector(mouse_postion()) - self.origin
+
+		if distance_to_origin.x > 0:
+			col = int(distance_to_origin.x / TILE_SIZE)
+		else:
+			col = int(distance_to_origin.x / TILE_SIZE) - 1
+
+		if distance_to_origin.y > 0:
+			row = int(distance_to_origin.y / TILE_SIZE)
+		else:
+			row = int(distance_to_origin.y / TILE_SIZE) - 1
+
+		return col, row
 
 	### FUNCTIONS TO HANDLE INPUTS
 	def event_loop(self):
@@ -40,6 +58,7 @@ class Editor:
 			self.pan_input(event) # pass the event to pan_input to detect if user wants to pan the editor area and act accordingly
 			self.selection_hotkeys(event)
 			self.menu_click(event)
+			self.canvas_add(event)
 
 	def pan_input(self, event):
 		# Check if user wants to pan by using middle mouse button (pressed / released)
@@ -76,6 +95,18 @@ class Editor:
 		if event.type == pygame.MOUSEBUTTONDOWN and self.menu.menu_area.collidepoint(mouse_postion()):
 			self.selection_index = self.menu.click(mouse_postion(), mouse_buttons())
 
+	def canvas_add(self, event):
+		if mouse_buttons()[0] and not self.menu.menu_area.collidepoint(mouse_postion()): # if we are lef clicking outside the menu area
+			current_cell = self.get_current_cell()
+			
+			if current_cell != self.last_selected_cell:
+				if current_cell in self.canvas_data:
+					self.canvas_data[current_cell].add_id(self.selection_index)
+				else:
+					self.canvas_data[current_cell] = CanvasTile(self.selection_index)
+				
+				self.last_selected_cell = current_cell
+
 	### FUNCTIONS TO DRAW THINGS
 	def draw_grid_lines(self):
 		num_cols = WINDOW_WIDTH // TILE_SIZE
@@ -109,3 +140,33 @@ class Editor:
 		self.draw_grid_lines()
 		pygame.draw.circle(self.editor_display_surface, 'red', self.origin, 10)
 		self.menu.display(self.selection_index)
+
+class CanvasTile:
+	def __init__(self, tile_id):
+
+		## terrain
+		self.has_terrain = False
+		self.terrain_neighbours = []
+
+		## water
+		self.has_water = False
+		self.water_on_top = False
+
+		## coin
+		self.coin = None # can be equal to 4, 5, or 6
+
+		## enemy
+		self.enemy = None
+
+		## objects
+		self.objects = []
+
+		self.add_id(tile_id)
+
+	def add_id(self, tile_id):
+		options = {key: value['style'] for key, value in EDITOR_DATA.items()}
+		match options[tile_id]:
+			case 'terrain': self.has_terrain = True
+			case 'water': self.has_water = True
+			case 'coin': self.coin = tile_id
+			case 'enemy': self.enemy = tile_id
