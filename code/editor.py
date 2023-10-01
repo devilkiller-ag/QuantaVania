c6108d261167f816ecba8e52ef8c37277732dac2
devilkiller-ag <ashmitgupta.official@gmail.com>
2023-10-01 10:21:09 +0530

    added functionality for objects(player, trees) in editor

--- code/editor.py
+++ code/editor.py
@@ -8,6 +8,7 @@ from pygame.image import load as loadImage
 from settings import *
 from support import *
 from menu import Menu
+from timer import Timer
 
 class Editor:
 	def __init__(self, land_tiles):
@@ -34,12 +35,36 @@ class Editor:
 		self.selection_index = 2 # By Default set to 2, i.e., terrain
 		self.last_selected_cell = None
 
-		## menu
+		## Menu
 		self.menu = Menu()
+
+		## Objects/Sprites: Player, Trees
+		self.canvas_objects = pygame.sprite.Group()
+		self.object_drag_active = False
+		self.object_timer = Timer(OBJECT_PLACING_DELAY_TIME) # To restrict player to draw another object (tree) just after placing first one. This will avoid the placing of thousands of objects (trees) on just draging the mouse after placing the first object (tree).
+
+		# Player
+		CanvasObject(
+			pos = (200, WINDOW_HEIGHT / 2), 
+			frames = self.animations[0]['frames'], 
+			tile_id = 0, 
+			origin = self.origin, 
+			group = self.canvas_objects
+		)
+
+		# Sky
+		self.sky_handle = CanvasObject(
+			pos = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), 
+			frames = [self.sky_handle], 
+			tile_id = 1, 
+			origin = self.origin, 
+			group = self.canvas_objects
+		)
 
 	### SUPPORT FUNCTIONS
 	def import_graphics(self):
-		self.water_bottom = loadImage('graphics/terrain/water/water_bottom.png')
+		self.water_bottom = loadImage('graphics/terrain/water/water_bottom.png').convert_alpha()
+		self.sky_handle = loadImage('graphics/cursors/handle.png').convert_alpha()
 		
 		# import animations
 		self.animations = {} # Ex: {3: {'frame index': 0, 'frames': [graphics_surfaces_01, graphics_surface_02], 'length': 2}}
@@ -66,6 +91,11 @@ class Editor:
 			row = int(distance_to_origin.y / TILE_SIZE) - 1
 
 		return col, row
+
+	def mouse_on_which_object(self):
+		for sprite in self.canvas_objects:
+			if sprite.rect.collidepoint(mouse_postion()):
+				return sprite
 
 	def check_neighbors(self, cell_position):
 		# create a local cluster of cells around the target cell (so that we only need to check only those 9 tiles in the cluster and not all the tiles)
@@ -115,6 +145,8 @@ class Editor:
 			self.selection_hotkeys(event)
 			self.menu_click(event)
 
+			self.object_drag(event)
+
 			self.canvas_add()
 			self.canvas_remove()
 
@@ -137,7 +169,9 @@ class Editor:
 		
 		# panning update
 		if self.pan_active:
-			self.origin = vector(mouse_postion()) - self.pan_offset
+			self.origin = vector(mouse_postion()) - self.pan_offset # update origin
+			for sprite in self.canvas_objects: # update position of all sprites
+				sprite.pan_position(self.origin)
 
 	def selection_hotkeys(self, event):
 		if event.type == pygame.KEYDOWN:
@@ -154,20 +188,41 @@ class Editor:
 			self.selection_index = self.menu.click(mouse_postion(), mouse_buttons())
 
 	def canvas_add(self):
-		if mouse_buttons()[0] and not self.menu.menu_area.collidepoint(mouse_postion()): # if we are left clicking outside the menu area
+		if mouse_buttons()[0] and not self.menu.menu_area.collidepoint(mouse_postion()) and not self.object_drag_active: # if we are left clicking outside the menu area and we have not selected any object(Player, Trees)
 			current_cell = self.get_current_cell()
+
+			if EDITOR_DATA[self.selection_index]['type'] == 'tile': ## ADD TILE
+				if current_cell != self.last_selected_cell:
+					if current_cell in self.canvas_data:
+						self.canvas_data[current_cell].add_id(self.selection_index)
+					else:
+						self.canvas_data[current_cell] = CanvasTile(self.selection_index)
+					
+					self.check_neighbors(current_cell)
+					self.last_selected_cell = current_cell
 			
-			if current_cell != self.last_selected_cell:
-				if current_cell in self.canvas_data:
-					self.canvas_data[current_cell].add_id(self.selection_index)
-				else:
-					self.canvas_data[current_cell] = CanvasTile(self.selection_index)
-				
-				self.check_neighbors(current_cell)
-				self.last_selected_cell = current_cell
+			else: ## ADD CANVAS OBJECT
+				if not self.object_timer.active:
+					CanvasObject(
+						pos = mouse_postion(), 
+						frames = self.animations[self.selection_index]['frames'], 
+						tile_id = self.selection_index, 
+						origin = self.origin, 
+						group = self.canvas_objects
+					)
+					self.object_timer.activate()
 
 	def canvas_remove(self):
+
 		if mouse_buttons()[2] and not self.menu.menu_area.collidepoint(mouse_postion()): # if we are right clicking outside the menu area
+			
+			## delete objects
+			selected_object = self.mouse_on_which_object()
+			if selected_object:
+				if EDITOR_DATA[selected_object.tile_id]['style'] not in ['player', 'sky']: # user should not be able to delete hero(player) or sky_handle
+					selected_object.kill()
+			
+			## delete tiles
 			if self.canvas_data:
 				current_cell = self.get_current_cell()
 				if current_cell in self.canvas_data:
@@ -177,6 +232,19 @@ class Editor:
 						del self.canvas_data[current_cell]
 					
 					self.check_neighbors(current_cell)
+
+	def object_drag(self, event):
+		if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[0]:
+			for sprite in self.canvas_objects:
+				if sprite.rect.collidepoint(event.pos):
+					sprite.start_drag()
+					self.object_drag_active = True
+		
+		if event.type == pygame.MOUSEBUTTONUP and self.object_drag_active:
+			for sprite in self.canvas_objects:
+				if sprite.selected:
+					sprite.end_drag(self.origin)
+					self.object_drag_active = False
 
 	### FUNCTIONS TO DRAW THINGS
 	def draw_grid_lines(self):
@@ -202,6 +270,7 @@ class Editor:
 		self.editor_display_surface.blit(self.grid_lines_surface, (0, 0))
 
 	def draw_level(self):
+		# Draw Tiles (Terrain, Water, Coin, Enemy)
 		for cell_pos, tile in self.canvas_data.items():
 			pos = self.origin + vector(cell_pos) * TILE_SIZE
 
@@ -237,12 +306,18 @@ class Editor:
 				enemy_rect_area = enemy_surface.get_rect(midbottom=(pos[0] + TILE_SIZE // 2, pos[1] + TILE_SIZE)) # to make the enemies touch the bottom of the grid_box/tile
 				self.editor_display_surface.blit(enemy_surface, enemy_rect_area)
 
+		# Draw Objects (Player, Trees)
+		self.canvas_objects.draw(self.editor_display_surface)
+
 	### FUNCTION TO RUN & UPDATE EVERYTHING
 	def run(self, dt):
 		self.event_loop()
 
 		# updating
 		self.animation_update(dt)
+		self.canvas_objects.update(dt)
+		self.object_timer.update()
+
 
 		# drawing
 		self.editor_display_surface.fill('white')
@@ -270,11 +345,7 @@ class CanvasTile:
 		## enemy
 		self.enemy = None # can be equal to 7, 8, 9, or 10
 
-		## objects
-		self.objects = []
-
 		self.add_id(tile_id)
-
 
 	def add_id(self, tile_id):
 		options = {key: value['style'] for key, value in EDITOR_DATA.items()}
@@ -296,3 +367,50 @@ class CanvasTile:
 	def check_content(self):
 		if not self.has_terrain and not self.has_water and not self.coin and not self.enemy:
 			self.is_empty = True
+
+
+class CanvasObject(pygame.sprite.Sprite):
+	def __init__(self, pos, frames, tile_id, origin, group):
+		super().__init__(group)
+		self.tile_id = tile_id
+
+		## animation
+		self.frames = frames
+		self.frame_index = 0
+
+		self.image = self.frames[self.frame_index]
+		self.rect = self.image.get_rect(center = pos)
+
+		## movement
+		self.distance_to_origin = vector(self.rect.topleft) - origin
+		self.selected = False
+		self.mouse_offset = vector()
+	
+	## FUNCTIONS TO HANDLE OBJECT DRAG
+	def start_drag(self):
+		self.selected = True
+		self.mouse_offset = vector(mouse_postion()) - vector(self.rect.topleft)
+
+	def drag(self):
+		if self.selected:
+			self.rect.topleft = mouse_postion() - self.mouse_offset 
+
+	def end_drag(self, origin):
+		self.selected = False
+		self.distance_to_origin = vector(self.rect.topleft) - origin
+
+	## FUNCTIONS TO HANDLE ANIMATIONS
+	def animate(self, dt):
+		self.frame_index += ANIMATION_SPEED * dt
+		if self.frame_index >= len(self.frames):
+			self.frame_index = 0
+		self.image = self.frames[int(self.frame_index)]
+		self.rect = self.image.get_rect(midbottom = self.rect.midbottom)
+
+	## FUNCTIONS TO HANDLE OBJECT MOVEMENT ALONG WITH CANVAS DRAG
+	def pan_position(self, origin):
+		self.rect.topleft = origin + self.distance_to_origin # change the positon of object according to the pan movement
+
+	def update(self, dt):
+		self.animate(dt)
+		self.drag()
