5a9230c12ebb4924afd71106e45ffaf9474e56b7
devilkiller-ag <ashmitgupta.official@gmail.com>
2023-10-04 19:29:31 +0530

    added logic to add I, X, Y, Z, RX, RY, RZ, CX, CY, CZ, CCZ Gate to the quantum circuit

--- code/quantum_circuit.py
+++ code/quantum_circuit.py
@@ -1,5 +1,5 @@
 import pygame
-import qiskit
+from qiskit import QuantumRegister, QuantumCircuit
 import numpy as np
 
 from pygame.image import load as loadImage
@@ -44,18 +44,18 @@ class QuantumCircuitGridMarker(pygame.sprite.Sprite):
         self.rect = self.qc_bg_surface.get_rect()
 
 class QuantumCircuitGridNode:
-    def __init__(self, node_type, radians = 0.0, ctrl_above = -1, ctrl_below = -1, swap = -1):
-        self.node_type = node_type # What Gate is at this node
-        self.radians = radians # If radian != 0 then this node have a U(theta) gate; Ex:- RX, RY, RZ
-        self.ctrl_above = ctrl_above # If Ctrl_above > 0; then this node is a controlled gate with the control qubit above it
-        self.ctrl_below = ctrl_below # If Ctrl_below > 0; then this node is a controlled gate with the control qubit below it
+    def __init__(self, gate_type, rotation_angle = 0.0, first_ctrl = -1, second_ctrl = -1, swap = -1):
+        self.gate_type = gate_type # What Gate is at this node
+        self.rotation_angle = rotation_angle # If radian != 0 then this node have a U(theta) gate; Ex:- RX, RY, RZ
+        self.first_ctrl = first_ctrl # If first_ctrl > 0; then this node is a controlled gate with one control node
+        self.second_ctrl = second_ctrl # If second_ctrl > 0; then this node is a controlled gate with two control nodes
         self.swap = swap # If swap != -1 then this node have a swap gate
 
     def __str__(self):
-        string = "Type: " + str(self.node_type)
-        string += ", radians: " + str(self.radians) if self.radians != 0 else ""
-        string += ", ctrl_a: " + str(self.ctrl_above) if self.ctrl_above != -1 else ""
-        string += ", ctrl_b: " + str(self.ctrl_below) if self.ctrl_below != -1 else ""
+        string = "Type: " + str(self.gate_type)
+        string += ", rotation_angle: " + str(self.rotation_angle) if self.rotation_angle != 0 else ""
+        string += ", ctrl_a: " + str(self.first_ctrl) if self.first_ctrl != -1 else ""
+        string += ", ctrl_b: " + str(self.second_ctrl) if self.second_ctrl != -1 else ""
         return string
 
 class QuantumCircuitGridGate(pygame.sprite.Sprite):
@@ -85,35 +85,35 @@ class QuantumCircuitGridGate(pygame.sprite.Sprite):
         elif gate == GATES['X']:
             node = self.qc_grid_model.get_node(self.wire, self.column)
             # Check if this is a CNOT Gate
-            if node.ctrl_above >= 0 or node.ctrl_below >= 0:
+            if node.first_ctrl >= 0 or node.second_ctrl >= 0:
                 if self.wire > max(node.ctrl_a, node.ctrl_b): # If target wire is below control wire
                     self.gate_surface, self.gate_rect = self.import_gate("not_gate_below_ctrl.png", -1)
                 else: # If target wire is above control wire
                     self.gate_surface, self.gate_rect = self.import_gate("not_gate_above_ctrl.png", -1)
-            elif node.radians != 0: # Else If this is a RX Gate
+            elif node.rotation_angle != 0: # Else If this is a RX Gate
                 self.gate_surface, self.gate_rect = self.import_gate("rx_gate.png", -1)
                 # Draw the value of theta as an arc of a circle 
-                pygame.draw.arc(self.gate_surface, QUANTUM_GATE_PHASE_COLOR, self.gate_rect, 0, node.radians % (2 * np.pi), 4)
+                pygame.draw.arc(self.gate_surface, QUANTUM_GATE_PHASE_COLOR, self.gate_rect, 0, node.rotation_angle % (2 * np.pi), 4)
             else: # Else if this is a normal X Gate
                 self.gate_surface, self.gate_rect = self.import_gate("x_gate.png", -1)
         
         elif gate == GATES['Y']:
             node = self.qc_grid_model.get_node(self.wire, self.column)
             # Check if this is a RY Gate
-            if node.radians != 0:
+            if node.rotation_angle != 0:
                 self.gate_surface, self.gate_rect = self.import_gate("ry_gate.png", -1)
                 # Draw the value of theta as an arc of a circle 
-                pygame.draw.arc(self.gate_surface, QUANTUM_GATE_PHASE_COLOR, self.gate_rect, 0, node.radians % (2 * np.pi), 4)
+                pygame.draw.arc(self.gate_surface, QUANTUM_GATE_PHASE_COLOR, self.gate_rect, 0, node.rotation_angle % (2 * np.pi), 4)
             else: # Else if this is a normal Y Gate
                 self.gate_surface, self.gate_rect = self.import_gate("y_gate.png", -1)
         
         elif gate == GATES['Z']:
             node = self.qc_grid_model.get_node(self.wire, self.column)
             # Check if this is a RY Gate
-            if node.radians != 0:
+            if node.rotation_angle != 0:
                 self.gate_surface, self.gate_rect = self.import_gate("rz_gate.png", -1)
                 # Draw the value of theta as an arc of a circle 
-                pygame.draw.arc(self.gate_surface, QUANTUM_GATE_PHASE_COLOR, self.gate_rect, 0, node.radians % (2 * np.pi), 4)
+                pygame.draw.arc(self.gate_surface, QUANTUM_GATE_PHASE_COLOR, self.gate_rect, 0, node.rotation_angle % (2 * np.pi), 4)
             else: # Else if this is a normal Y Gate
                 self.gate_surface, self.gate_rect = self.import_gate("z_gate.png", -1)
         
@@ -137,7 +137,7 @@ class QuantumCircuitGridGate(pygame.sprite.Sprite):
         
         elif gate == GATES['CTRL']:
             # Check if the target wire is above the control wire
-            if self.wire > self.qc_grid_model.get_control_wire_for_target_gate_at(self.wire, self.column):
+            if self.wire > self.qc_grid_model.get_wire_for_control_node_at(self.wire, self.column):
                 self.gate_surface, self.gate_rect = self.import_gate("ctrl_gate_bottom_wire.png", -1)
             else: # if the target wire is above the control wire
                 self.gate_surface, self.gate_rect = self.import_gate("ctrl_gate_top_wire.png", -1)
@@ -174,12 +174,77 @@ class QuantumCircuitGridModel():
 
     def set_node(self, wire, column, qc_grid_node):
         self.nodes[wire][column] = QuantumCircuitGrid(
-            qc_grid_node.node_type,
-            qc_grid_node.radians,
-            qc_grid_node.ctrl_above,
-            qc_grid_node.ctrl_below,
+            qc_grid_node.gate_type,
+            qc_grid_node.rotation_angle,
+            qc_grid_node.first_ctrl,
+            qc_grid_node.second_ctrl,
             qc_grid_node.swap
         )
+    
+    def get_node(self, wire, column):
+        return self.nodes[wire][column]
+
+    def get_gate_at_node(self, wire, column):
+        node = self.node[wire][column]
+        
+        if node and node.gate_type != GATES['EMPTY']: # If the node is already occupied
+            return node.gate_type # Return the gate occupying the node
+        
+        column_nodes = self.nodes[:, column]
+        for index, other_node in enumerate(column_nodes):
+            if index != wire and other_node:
+                # Check if the other_node is a control node
+                if other_node.first_ctrl == wire or other_node.second_ctrl == wire:
+                    return GATES['CTRL']
+                # Or if it is a swap node
+                elif other_node.swap == wire:
+                    return GATES['SWAP']
+        
+        # If no gate is present at the node return 'EMPTY'
+        return GATES['EMPTY']
+
+    def get_wire_for_control_node_at(self, control_wire, column):
+        control_wire = -1
+        column_nodes = self.nodes[:, column]
+
+        for index in range(self.num_qubits):
+            if index != control_wire:
+                other_node = column_nodes[index]
+                if other_node:
+                    if (other_node.first_ctrl == control_wire or other_node.second_ctrl == control_wire):
+                        control_wire = index
+                        print("Found ", self.get_gate_at_node(control_wire, column), " on wire ", control_wire)
+        
+        return control_wire
+
+    def compute_quantum_circuit(self):
+        """Create and Compute Quantum Circuit from Quantum Circuit Grid"""
+        qr = QuantumRegister(self.num_qubits, "q")
+        qc = QuantumCircuit(qr)
+
+        for column in range(self.num_columns):
+            for wire in range(self.num_qubits):
+                node = self.nodes[wire][column]
+                
+                if node:
+                    if node.gate_type == GATES['IDENTITY']:
+                        qc.i(qr[wire])
+
+                    elif node.gate_type == GATES['X']:
+                        if node.rotation_angle == 0: # Node have a normal X Gate
+                            if node.first_ctrl != -1: # If first control is active
+                                if node.second_ctrl != -1: # If second control is also active then node have a Toffoli Gate
+                                    qc.ccx(qr[node.frist_ctrl], qr[node.second_ctrl], qr[wire])
+                                else: # Node have a Controlled X Gate
+                                    qc.cx(qr[node.frist_ctrl], qr[wire])
+                            else: # If no control is active then the node have a Pauli X Gate
+                                qc.x(qr[wire])
+                        else: # If angle is not zero then it is a RX Gate
+                            qc.rx(node.rotation_angle, qr[wire])
+                    
+                    
+
+
 
 class QuantumCircuitGrid(pygame.sprite.RenderPlain):
     def __init__(self, position):
